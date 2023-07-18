851a746adab5e32ecd05855c405be4d6e085c528
chrjl <73675826+chrjl@users.noreply.github.com>
2023-07-17 18:51:46 -0700

    feat(VerticalCard): add component
    Vertical card components expected to take up half of the paper width.
    
    Renders four parameters (title, subtitle, description, highlights):
    - Heading line: left-aligned title span and right-aligned subtitle span,
      within the same div. Clearfix to expand the height of the div for
      alignment of the following elements.
    - Description: freeform description followed by list of highlights

---
+++ src/components/VerticalCard.jsx
@@ -0,0 +1,35 @@
+import PropTypes from 'prop-types';
+
+import * as verticalCard from './VerticalCard.module.css';
+
+function VerticalCard(props) {
+  const { title, subtitle, keywords, description, highlights } = props;
+  return (
+    <div className={verticalCard.container}>
+      <div className={verticalCard.heading}>
+        <span className={verticalCard.title}>{title}</span>
+        {subtitle && <span className={verticalCard.subtitle}>{subtitle}</span>}
+      </div>
+
+      <div className={verticalCard.description}>{description}</div>
+      <div className={verticalCard.highlights}>
+        {highlights && (
+          <ul>
+            {highlights.map((highlight, index) => (
+              <li key={index}>{highlight}</li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </div>
+  );
+}
+
+VerticalCard.propTypes = {
+  title: PropTypes.node.isRequired,
+  subtitle: PropTypes.node,
+  description: PropTypes.node,
+  highlights: PropTypes.arrayOf(PropTypes.node),
+};
+
+export default VerticalCard;
