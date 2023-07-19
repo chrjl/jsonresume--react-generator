558747b7aa329ff4495a808dcba3a59b6abdc241
chrjl <73675826+chrjl@users.noreply.github.com>
2023-07-19 15:53:59 -0700

    feat(HorizontalCard): add component
    Horizontal card components for single column layout
    
    Renders four parameters (title, subtitle, description, highlights):
    - Heading line: title and subtitle spans
    - Description: freeform description followed by list of highlights

---
+++ src/components/HorizontalCard.jsx
@@ -0,0 +1,37 @@
+import PropTypes from 'prop-types';
+
+import * as horizontalCard from './HorizontalCard.module.css';
+
+function HorizontalCard(props) {
+  const { title, subtitle, description, highlights } = props;
+  return (
+    <div className={horizontalCard.container}>
+      <div className={horizontalCard.header}>
+        <span className={horizontalCard.title}>{title}</span>
+        {subtitle && (
+          <span className={horizontalCard.subtitle}>{subtitle}</span>
+        )}
+      </div>
+
+      <div className={horizontalCard.description}>{description}</div>
+      <div className={horizontalCard.highlights}>
+        {highlights && (
+          <ul>
+            {highlights.map((highlight, index) => (
+              <li key={index}>{highlight}</li>
+            ))}
+          </ul>
+        )}
+      </div>
+    </div>
+  );
+}
+
+HorizontalCard.propTypes = {
+  title: PropTypes.node.isRequired,
+  subtitle: PropTypes.node,
+  description: PropTypes.node,
+  highlights: PropTypes.arrayOf(PropTypes.node),
+};
+
+export default HorizontalCard;
