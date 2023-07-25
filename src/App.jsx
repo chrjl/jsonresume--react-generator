c57d522f6528f9d14640c60119dde462ffa96d6e
chrjl <73675826+chrjl@users.noreply.github.com>
2023-07-24 19:36:46 -0700

    fix(App): initialize App with Effect
    Move the fetch inside the App component, and initialize using an Effect
    that fetches the sample.
    
    Fetching the sample with a top-level await to set it to the initial
    state caused problems on build/export. Instead, using Effect inside the
    component will hopefully be able to synchronize the resume state to the
    public asset while still only running once, when the App component is
    rendered.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 import './styles/paper.css';
 import './styles/App.css';
@@ -13,11 +13,23 @@ import Projects from './pages/Projects';
 import Skills from './pages/Skills';
 import Work from './pages/Work';
 
-const fetchSample = await fetch('resume.json');
-const sample = await fetchSample.json();
+let didInit = false;
+const defaultUrl = 'resume.json';
 
 function App() {
-  const [resume, setResume] = useState(sample);
+  const [resume, setResume] = useState({});
+
+  // initialize app by fetching a sample
+  useEffect(() => {
+    if (didInit) return;
+
+    fetch(defaultUrl)
+      .then((res) => res.json(res))
+      .then((result) => setResume(result))
+      .then(() => {
+        didInit = true;
+      });
+  }, []);
 
   if (resume?.basics?.name) {
     document.title = `Resume | ${resume.basics.name}`;
