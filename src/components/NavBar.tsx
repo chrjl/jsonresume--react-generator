f9b53a3a170e6545118a8304807022d34f03c00d
chrjl <73675826+chrjl@users.noreply.github.com>
2023-08-01 12:50:33 -0700

    refactor(NavBar): TypeScript

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,123 @@
+import { useState } from 'react';
+
+import * as styles from './NavBar.module.css';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+
+export default function NavBar({ defaultUrl, setResume }: NavBarProps) {
+  return (
+    <nav className={styles.container}>
+      <div>
+        <DataSourcePicker defaultUrl={defaultUrl} setResume={setResume} />
+      </div>
+
+      <div className={styles.icons}>
+        <a
+          href="https://github.com/chrjl/jsonresume--react-generator"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FontAwesomeIcon icon={faGithub} style={{ color: '#ffffff' }} />
+        </a>
+      </div>
+    </nav>
+  );
+}
+
+function DataSourcePicker({ defaultUrl, setResume }: DataSourcePickerProps) {
+  const [sourceType, setSourceType] = useState('url');
+  const [urlSource, setUrlSource] = useState(defaultUrl);
+
+  return (
+    <>
+      <select
+        name="source-type"
+        value={sourceType}
+        onChange={(event) => {
+          setSourceType(event.target.value);
+        }}
+      >
+        <optgroup label="Source">
+          <option value="url">URL</option>
+          <option value="file">File</option>
+        </optgroup>
+      </select>
+
+      {sourceType == 'url' && (
+        <form
+          name="source"
+          onSubmit={(event) => {
+            event.preventDefault();
+
+            const url = (event.target as HTMLFormElement).url.value;
+            submitUrl(url);
+          }}
+          onReset={() => submitUrl(defaultUrl)}
+        >
+          <input name="url" type="text" defaultValue={urlSource}></input>
+          <button type="submit">Fetch</button>
+          <button type="reset">Default</button>
+        </form>
+      )}
+
+      {sourceType === 'file' && (
+        <input
+          type="file"
+          name="file"
+          accept="application/json"
+          onChange={(event) => {
+            event.preventDefault();
+
+            if (event.target.files === null) return;
+            const file = event.target.files[0];
+            console.log(file);
+
+            submitFile(file);
+          }}
+        ></input>
+      )}
+    </>
+  );
+
+  async function submitUrl(url: string) {
+    try {
+      const fetchResume = await fetch(url);
+
+      if (fetchResume.status >= 400) {
+        throw new Error(fetchResume.statusText);
+      }
+
+      setResume(await fetchResume.json());
+      setUrlSource(url);
+    } catch (error) {
+      alert(error);
+    }
+  }
+
+  async function submitFile(file: File) {
+    const reader = new FileReader();
+
+    reader.readAsText(file, 'utf8');
+    reader.onload = () => {
+      try {
+        const { result } = reader;
+
+        setResume(JSON.parse(result as string));
+      } catch (err) {
+        alert(err.message);
+      }
+    };
+  }
+}
+
+interface NavBarProps {
+  defaultUrl: string;
+  setResume: React.Dispatch<React.SetStateAction<string>>;
+  // setShowSideBar: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+interface DataSourcePickerProps {
+  defaultUrl: string;
+  setResume: React.Dispatch<React.SetStateAction<string>>;
+}
