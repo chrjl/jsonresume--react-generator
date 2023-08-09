1e147014a1b20d4b572e5b0dcd6e95a46fd5e5ad
chrjl <73675826+chrjl@users.noreply.github.com>
2023-08-09 11:35:33 -0700

    feat(Experience): add projects subfield
    Add projects as a subcategory of Experience, utilizing ProjectCard
    (VerticalCard) and grid layout.

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -4,17 +4,27 @@ import PropTypes from 'prop-types';
 import ReactMarkdown from 'react-markdown';
 import DefinitionTable from '../components/DefinitionTable';
 import { WorkCard } from './Work';
+import { ProjectCard } from './Projects';
+
+import * as styles from './Experience.module.css';
 
 function Experience({ data }) {
   return (
     <section id="experience">
       <h3>Experience</h3>
-      {data.map(({ categories, ...workItem }, index) => (
+      {data.map(({ categories, projects, ...workItem }, index) => (
         <Fragment key={index}>
           <WorkCard {...workItem} />
           {categories && (
-            <div style={{ paddingLeft: '2em' }}>
+            <div className={styles.categories}>
               <CategoricalDetails categories={categories} />
+            </div>
+          )}
+          {projects && (
+            <div className={styles.projects}>
+              {projects.map((project, index) => (
+                <ProjectCard key={index} {...project} />
+              ))}
             </div>
           )}
         </Fragment>
